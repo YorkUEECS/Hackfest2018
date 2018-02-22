5f8e7aaf40bf1cdf458280903c6803f3a0aff66d
aemc <aemlcruz@gmail.com>
2018-02-21 23:58:33 -0500

    4.0 converter works

--- src/app/app.js
+++ src/app/app.js
@@ -14,13 +14,12 @@ class AppCtrl {
   constructor() {
     this.url = 'https://github.com/preboot/angular-webpack';
     this.numberG;
+    this.numberG2;
     this.rowLength = 5;
     this.appArr = [];
+    this.appArr2 = [];
     this.courseGradeValues = {};
     this.courseCredit = {};
-    this.creditArr = [];
-    this.totalCredit = 0.0;
-    this.sumGradeCreditProd = 0;
     this.GPA;
 
     this.numGArr = [];
@@ -37,17 +36,16 @@ class AppCtrl {
   multCreditGrade(numGrade, credit, index) {
       if (credit !== undefined && numGrade !== undefined) {
           this.numGArr[index] = (parseFloat(credit) * parseFloat(numGrade));
-          console.log(index)
-          console.log(this.numGArr[index])
       }
   }
 
   calculateGPA() {
+      let totalcreditz = 0;
+
       const total = this.numGArr.reduce((total, curr) => {
           return total + curr;
       }, 0);
 
-      let totalcreditz = 0;
       Object.keys(this.courseCredit).forEach(key => {
           totalcreditz += parseFloat(this.courseCredit[key]);
       });
@@ -55,14 +53,55 @@ class AppCtrl {
       this.GPA = (total / totalcreditz).toFixed(2);
   }
 
-  addtoTotalCredit(credit) {
-      this.totalCredit += parseFloat(credit);
-  }
-
-  calcToYorkGrade(letterG, index) {
+  calcToOtherGrade(letterG, index) {
       switch(letterG.toUpperCase()) {
-
+          case "A+":
+              this.numberG2 = 4;
+              break;
+          case "A":
+              this.numberG2 = 4;
+              break;
+          case "A-":
+              this.numberG2 = 3.7;
+              break;
+          case "B+":
+              this.numberG2 = 3.3;
+              break;
+          case "B":
+              this.numberG2 = 3;
+              break;
+          case "B-":
+              this.numberG2 = 2.7;
+              break;
+          case "C+":
+              this.numberG2 = 2.3;
+              break;
+          case "C":
+              this.numberG2 = 2;
+              break;
+          case "C-":
+              this.numberG2 = 1.7;
+              break;
+          case "D+":
+              this.numberG2 = 1.3;
+              break;
+          case "D":
+              this.numberG2 = 1;
+              break;
+          case "D-":
+              this.numberG2 = 0.7;
+              break;
+          case "E":
+              this.numberG2 = 0;
+              break;
+          case "F":
+              this.numberG2 = 0;
+              break;
+          default:
+              this.numberG2 = null;
       }
+
+      this.appArr2[index] = this.numberG2;
   }
 
   calcToYorkGrade(letterG, index) {
@@ -100,11 +139,10 @@ class AppCtrl {
           default:
               this.numberG = null;
       }
-      this.appArr[index] = this.numberG;
 
-      console.log(this.courseCredit['field'+index])
-      console.log(this.appArr[index])
-      this.multCreditGrade(this.appArr[index], this.courseCredit['field'+index], index)
+      this.appArr[index] = this.numberG;
+      this.multCreditGrade(this.appArr[index], this.courseCredit['field'+index], index);
+      this.calcToOtherGrade(letterG, index)
   }
 }
 
